f840b8e09363909b426cc214d17d28fba1b4faf8
FreakJLH2025 <joshua.luja@gmail.com>
2026-05-24 18:54:41 +0000

    React.ts

---
+++ TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,23 @@
+// React.ts
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+  // Declaration merging: add optional attribute
+  export interface Teacher {
+    experienceTeachingReact?: number;
+  }
+
+  export class React extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for React";
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher.experienceTeachingReact || this.teacher.experienceTeachingReact <= 0) {
+        return "No available teacher";
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
