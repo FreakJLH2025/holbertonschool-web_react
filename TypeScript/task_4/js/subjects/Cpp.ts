0006b47252ea7516530361cb41b23f7236b4724d
FreakJLH2025 <joshua.luja@gmail.com>
2026-05-24 18:54:00 +0000

    Cpp.ts

---
+++ TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,23 @@
+// Cpp.ts
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+  // Declaration merging: add optional attribute
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for Cpp";
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher.experienceTeachingC || this.teacher.experienceTeachingC <= 0) {
+        return "No available teacher";
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
